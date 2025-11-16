a091fe142ab02abb1f5b1049b134e21603e0e9cd
miguel.silva <miguelss616@outlook.com.br>
2025-11-16 13:40:20 -0300

    feat: habilitar Async, Retry e Scheduling no EmprestimoService
    Habilitar as anotações `@EnableAsync`, `@EnableRetry` e `@EnableScheduling` na aplicação.
    
    Além disso, ajustar configurações no `application.properties` incluindo porta do servidor, conexão com banco de dados PostgreSQL, propriedades do JPA e propriedades de JWT.
    
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/emprestimo_service/EmprestimoServiceApplication.java
+++ src/main/java/br/com/scripta_api/emprestimo_service/EmprestimoServiceApplication.java
@@ -2,12 +2,16 @@ package br.com.scripta_api.emprestimo_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableRetry
+@EnableScheduling
 @SpringBootApplication
 public class EmprestimoServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EmprestimoServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmprestimoServiceApplication.class, args);
+    }
 }
