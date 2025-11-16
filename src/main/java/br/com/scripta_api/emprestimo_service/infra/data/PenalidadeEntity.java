bddcc1899be48c82f94ee86533bd54f05acdf40d
miguel.silva <miguelss616@outlook.com.br>
2025-11-16 14:03:24 -0300

    feat: adicionar entidade e repositório para penalidades
    Criar a classe `PenalidadeEntity` com mapeamento JPA e o repositório `PenalidadeEntityRepository` para permitir persistência e consulta de penalidades no banco.
    
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

---
+++ src/main/java/br/com/scripta_api/emprestimo_service/infra/data/PenalidadeEntity.java
@@ -0,0 +1,23 @@
+package br.com.scripta_api.emprestimo_service.infra.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity(name = "penalidade")
+@Table(name = "penalidade")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PenalidadeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long alunoId;
+    private LocalDate dataFimPenalidade;
+}
---
+++ src/main/java/br/com/scripta_api/emprestimo_service/infra/gateways/PenalidadeEntityRepository.java
@@ -0,0 +1,12 @@
+package br.com.scripta_api.emprestimo_service.infra.gateways;
+
+import br.com.scripta_api.emprestimo_service.infra.data.PenalidadeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface PenalidadeEntityRepository extends JpaRepository<PenalidadeEntity, Long> {
+
+    Optional<PenalidadeEntity> findByAlunoIdAndDataFimPenalidadeAfter(Long alunoId, LocalDate today);
+}
