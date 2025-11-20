687a11b1f52ed08a6330bbdd83c8174bfbdbbf3b
miguel.silva <miguelss616@outlook.com.br>
2025-11-20 09:36:31 -0300

    feat: Adiciona construtor vazio e remove record dos Dtos para uso do Data no lombok
    Signed-off-by: miguel.silva <miguelss616@outlook.com.br>

--- src/main/java/br/com/scripta_api/emprestimo_service/dto/SolicitacaoEmprestimoRequest.java
+++ src/main/java/br/com/scripta_api/emprestimo_service/dto/SolicitacaoEmprestimoRequest.java
@@ -1,8 +1,13 @@
 package br.com.scripta_api.emprestimo_service.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-public record SolicitacaoEmprestimoRequest(
-        @NotNull Long livroId
-) {
+@Data
+public class SolicitacaoEmprestimoRequest {
+    private @NotNull Long livroId;
+
+    public SolicitacaoEmprestimoRequest() {
+
+    }
 }
